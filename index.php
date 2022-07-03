481dec48a52fa1fcb00226c6e05dcbfccbde3e44
Mohit <mohitmathur9968@outlook.com>
2022-07-03 11:13:29 +0530

    start

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?php 
-
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>let's talk</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Realtime chat app </header>
+            <form action="#">
+                <div class="error-txt">this is an error msg</div>
+                <div class="name-details">
+                    <div class="field">
+                        <label for="">First Name</label>
+                        <input type="text" placeholder="first Name">
+                    </div>
+                </div>
+            </form>
+        </section>
+    </div>
+</body>
+</html>
